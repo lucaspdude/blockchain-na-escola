a811344807ec2d3966dd3b1c1b6a1a09286a0506
Lucas Pacheco <lucaspdude@gmail.com>
2022-05-04 08:40:18 -0300

    hotfix/title

--- layouts/meta.tsx
+++ layouts/meta.tsx
@@ -34,6 +34,7 @@ const Meta: FunctionComponent<MetaProps> = ({
       ></meta>
       <meta charSet="utf-8"></meta>
       <link rel="icon" href="/favicon.ico"></link>
+      <title>{title}</title>
 
       <NextSeo
         title={title}
